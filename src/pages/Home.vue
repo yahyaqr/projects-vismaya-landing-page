67692955ad27095315af2c6394f36a27268cf364
yahyaqr <aqrom.yahya75@gmail.com>
2025-02-03 16:16:30 +0700

    Fix

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -24,7 +24,7 @@ import HeroSection from '../components/Home/Hero.vue';
 import ClientsSection from '../components/Home/Clients.vue';
 import ServicesSection from '../components/Home/Services/Index.vue';
 import AboutSection from '../components/Home/About.vue';
-import TestimonialsSection from '../components/Home/Testimonials/index.vue';
+import TestimonialsSection from '../components/Home/Testimonials/Index.vue';
 import ShowcaseSection from '../components/Home/Showcase.vue';
 import CTASection from '../components/Home/CTA.vue';
 import Footer from '../components/Footer.vue';
